d560ee7164f1740f0f25cb79628c4b4f5a7dfaa2
Dorin Ilușca <dorin.ilusca@stefanini.com>
2022-10-31 16:23:12 +0200

    fix: function naming

--- data-types/numbers.js
+++ data-types/numbers.js
@@ -1,14 +1,14 @@
-export function RandomIntegerTASK() {
-
+export function RandomIntegerBetweenMinAndMax() {
   /**
     Create a function randomInteger(min, max) that generates a random integer number from min to max
     including both min and max as possible values.
    */
 
   function randomInteger(min, max) {
-    let rand = min + Math.random() * (max - min + 1)
-    return Math.floor(rand)
+    let rand = min + Math.random() * (max - min + 1);
+    let res = Math.floor(rand);
+    return `Random integer between ${min}-${max} is: ${res}`;
   }
-  alert(randomInteger(1, 5))
 
+  alert(randomInteger(1, 5));
 }
--- objects-the-basics/constructor-new.js
+++ objects-the-basics/constructor-new.js
@@ -1,40 +1,37 @@
-export function calculatorEx() {
-
+export function CreateNewCalculator() {
   function Calculator() {
-    this.read = function() {
-      this.a = +prompt("a = ", 0)
-      this.b = +prompt("b = ", 0)
-    }
-    this.sum = function() {
-      return this.a + this.b
-    }
-    this.mul = function() {
-      return this.a * this.b
-    }
+    this.read = function () {
+      this.a = +prompt("a = ", 0);
+      this.b = +prompt("b = ", 0);
+    };
+    this.sum = function () {
+      return this.a + this.b;
+    };
+    this.mul = function () {
+      return this.a * this.b;
+    };
   }
-  let calculator = new Calculator()
-  calculator.read()
+  let calculator = new Calculator();
+  calculator.read();
 
-  alert( "Sum=" + calculator.sum() )
-  alert( "Mul=" + calculator.mul() )
-
+  alert("Sum=" + calculator.sum());
+  alert("Mul=" + calculator.mul());
 }
 
-export function accumulatorEx() {
+export function CreateNewAccumulator() {
   /** 
    Create a constructor function Accumulator(startingValue), that returns the sum of all user-entered values with the initial value startingValue
    */
   function Accumulator(startingValue) {
-    this.value = startingValue
-    this.read = function() {
-      this.value += +prompt("Nr to be added ", 0)
-    }
+    this.value = startingValue;
+    this.read = function () {
+      this.value += +prompt("Nr to be added ", 0);
+    };
   }
 
   let accumulator = new Accumulator(1);
-  accumulator.read()
-  accumulator.read()
+  accumulator.read();
+  accumulator.read();
 
-  alert(accumulator.value)
-  
+  alert(accumulator.value);
 }
