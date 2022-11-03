6485887562e02dc978b8ba2d1fa8ab03db0f3c83
Dorin Ilușca <dorin.ilusca@stefanini.com>
2022-11-03 11:01:14 +0200

    fix: styles

--- crud-app/app.js
+++ crud-app/app.js
@@ -4,16 +4,22 @@ const tableBody = document.querySelector("#data-users");
 
 const httpClient = new HttpClient({ baseUrl: "https://crudapi.co.uk/api/v1" });
 
+async function fetchUserDetails(id) {
+  const { data: user, isLoading } = await httpClient.get(`/users/${id}`);
+  if (user) console.log(user);
+}
+
 const updateUsersTable = async () => {
-  const span = document.querySelector("#loading");
+  const loader = document.querySelector("#loading");
   const usersTable = document.querySelector("#users-data-table");
 
-  span.textContent = ">>> Loading...";
+  loader.classList.add("show-loading");
 
   const { data: users, isLoading } = await httpClient.get("/users");
 
   if (!isLoading && users) {
-    span.textContent = "";
+    loader.classList.remove("show-loading");
+
     let fragment = new DocumentFragment();
 
     users.items.forEach((user) => {
@@ -23,13 +29,19 @@ const updateUsersTable = async () => {
           <td>${user.surname}</td>
           <td>${user.email}</td>
           <td>${user.birth}</td>
-          <td><a href="details.html" target="_blank">Show Details</a></td>
+          <td>
+            <button              
+              onclick="fetchUserDetails(${user._uuid})"
+            >
+              Show Details
+            </button>
+          </td>
         `;
 
       fragment.append(tr);
     });
 
-    tableBody.append(fragment ?? "UPS");
+    tableBody.append(fragment);
     usersTable.classList.remove("display-none");
   }
 };
@@ -44,7 +56,6 @@ document.addEventListener("DOMContentLoaded", updateUsersTable);
 //   var form = document.form.email.value;
 //   console.log(fname, lname, form);
 // };
-let posting = false;
 
 const createUserForm = document.getElementById("createUser");
 createUserForm.addEventListener("submit", async (e) => {
@@ -52,18 +63,20 @@ createUserForm.addEventListener("submit", async (e) => {
 
   let formData = new FormData(createUserForm);
   const formProps = [Object.fromEntries(formData)];
-  // console.log(formProps);
+  // let name = document.createUserForm.name.value;
+  // let surname = document.createUserForm.surnamename.value;
+  // let email = document.createUserForm.email.value;
 
-  posting = true;
   showToaster("Posting...!");
 
   const response = await httpClient.post("/users", formProps);
 
   if (response && !response.isLoading) {
-    posting = false;
     updateToaster("Posted successfully!", 2000);
   }
+
   updateLocalTable(formProps[0]);
+  createUserForm.reset();
 });
 
 const showToaster = (message) => {
@@ -88,7 +101,7 @@ const updateLocalTable = (formData) => {
           <td>${formData.surname}</td>
           <td>${formData.email}</td>
           <td>${formData.birth}</td>
-          <td><a href="details.html" target="_blank">Show Details</a></td>
+          <td><button>Show Details</button></td>
         `;
   tableBody.prepend(tr);
 };
