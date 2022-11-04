faabff177a1c4071dbbeb6024afe574009cabb73
Dorin Ilușca <dorin.ilusca@stefanini.com>
2022-11-04 18:40:10 +0200

    fix: toaster not updating properly

--- crud-app/scripts/toaster.js
+++ crud-app/scripts/toaster.js
@@ -7,9 +7,8 @@ export const showToaster = (message) => {
 
 export const updateToaster = (type = "ok", message, ms) => {
   const toaster = document.querySelector(".toaster");
-  if (toaster && toaster.textContent != "") toaster.textContent = message;
+  if (toaster) toaster.textContent = message;
   if (type == "error") {
-    console.log("error");
     toaster.style =
       "background: rgba(255, 29, 29, 0.785); border: 1px solid rgba(255, 29, 29, 0.585)";
   }
