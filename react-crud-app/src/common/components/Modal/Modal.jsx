29b43c88bad7190d39e5077351566b8f3a10139a
Dorin Ilușca <98060574+dorin-js@users.noreply.github.com>
2022-11-16 12:55:27 +0000

    fix: children as last in order

--- react-crud-app/src/common/components/Modal/Modal.jsx
+++ react-crud-app/src/common/components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './Modal.module.css';
 
-const Modal = ({ title, children, onClose }) => (
+const Modal = ({ title, onClose, children }) => (
   <div className={classes.modal}>
     <span className={classes.modalTitle}>{title}</span>
     <button type="button" className={classes.closeIcon} onClick={onClose}>
@@ -15,11 +15,10 @@ const Modal = ({ title, children, onClose }) => (
 Modal.propTypes = {
   title: PropTypes.string,
   children: PropTypes.node.isRequired,
-  onClose: PropTypes.func,
+  onClose: PropTypes.func.isRequired,
 };
 Modal.defaultProps = {
   title: '',
-  onClose: () => undefined,
 };
 
 export default Modal;
